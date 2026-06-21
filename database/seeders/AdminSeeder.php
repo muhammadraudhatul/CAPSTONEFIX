c09fee12cb97f055b398be18bf89de7b27d405c8
muhammadraudhatul <muhammadraudhatul@gmail.com>
2026-06-21 19:31:26 +0700

    Force HTTPS in production

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,7 +10,7 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
+        User::firstOrCreate()([
             'name'     => 'Admin',
             'nim'      => 'admin',
             'role'     => 'admin',
